5ffa4c3548a323652e5af091a13ee80b3347aa8a
rizatemizel <127015640+rizatemizel@users.noreply.github.com>
2024-05-29 22:07:24 +0000

    HW2 finished

--- 02-experiment-tracking/homework/hpo.py
+++ 02-experiment-tracking/homework/hpo.py
@@ -34,11 +34,19 @@ def run_optimization(data_path: str, num_trials: int):
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
     def objective(params):
+        
+        with mlflow.start_run():
 
-        rf = RandomForestRegressor(**params)
-        rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_val)
-        rmse = mean_squared_error(y_val, y_pred, squared=False)
+            rf = RandomForestRegressor(**params)
+            
+            mlflow.log_params(params)
+            
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_val)
+            rmse = mean_squared_error(y_val, y_pred, squared=False)
+            
+            mlflow.log_metric("RMSE", rmse)
+            
 
         return {'loss': rmse, 'status': STATUS_OK}
 
--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -5,9 +5,11 @@ import mlflow
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
-mlflow.set_tracking_uri("sqlite:///mlflow.db")
-mlflow.set_experiment("newyork_taxi_experiment")
+# Set the MLflow tracking URI
+mlflow.set_tracking_uri(uri="http://127.0.0.1:5000")
 
+# Set the experiment name
+mlflow.set_experiment("newyork_taxi_experiment")
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -19,26 +21,27 @@ def load_pickle(filename: str):
     default="./output",
     help="Location where the processed NYC taxi trip data was saved"
 )
-
-
-
 def run_train(data_path: str):
 
     with mlflow.start_run():
-        
+        # Enable MLflow auto-logging
+        mlflow.sklearn.autolog()
 
+        # Load training and validation data
         X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
         X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-        mlflow.sklearn.autolog()
-
+        # Create and train RandomForestRegressor model
         rf = RandomForestRegressor(max_depth=10, random_state=0)
         rf.fit(X_train, y_train)
 
+        # Predict on validation data
         y_pred = rf.predict(X_val)
 
+        # Calculate RMSE
         rmse = mean_squared_error(y_val, y_pred, squared=False)
 
+        # Log RMSE metric
         mlflow.log_metric("RMSE", rmse)
 
 if __name__ == '__main__':
